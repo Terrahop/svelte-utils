0aee4bf67777e6819cd91c7a6d03daffbb35903f
terrahop <alex@terrahop.dev>
2025-01-16 15:54:04 +0200

    feat(helpers): add random helper

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,1 +1,5 @@
+// eslint-disable-next-line unicorn/prefer-global-this -- Necessary
 export const isBrowser = typeof window !== 'undefined' && typeof document !== 'undefined'
+
+// eslint-disable-next-line sonarjs/pseudo-random -- Its safe
+export const random = () => Math.random().toString()
