cfbbdbe78bac3b7951b27314a55187896bd68223
terrahop <alex@terrahop.dev>
2025-02-25 15:02:03 +0200

    chore: update build

--- dist/actions/persist.d.ts
+++ dist/actions/persist.d.ts
@@ -4,7 +4,7 @@ interface BasePersistConfig {
      * The event to listen to for persisting the form state.
      * @default 'input'
      */
-    persistOn?: 'input' | 'change' | 'blur';
+    persistOn?: 'input' | 'change' | 'blur-xs';
     /**
      * Setting this to `false` will cause the password fields to be persisted. Highly discouraged.
      *
